3ed924ed038607c4d3f1fafdc9726dd721931e04
whysoakemiii <sujira.jamdee@gmail.com>
2023-03-30 00:38:11 +0700

    .......

--- CafeManagemnet/src/Dashboard.java
+++ CafeManagemnet/src/Dashboard.java
@@ -11,7 +11,7 @@ public class Dashboard extends JFrame{
     private JButton totalButton;
     private JButton receipButton;
     private JButton resetButton;
-    private JButton exiButton;
+    private JButton exitButton;
 
     public Dashboard() {
         setSize(1360, 790);
@@ -19,58 +19,57 @@ public class Dashboard extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setUndecorated(true);
 
+        // Title panel
+        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         titleLabel = new JLabel("Sunny Side Up! Cafe");
         titleLabel.setFont(new Font("Bahnschrift", Font.BOLD, 30));
+        titlePanel.add(titleLabel);
 
+        // Menu panel
+        JPanel menuPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
+        // Coffee names and picture squares
         nameCheckBox[0] = new JCheckBox("Capuccino");
         nameCheckBox[1] = new JCheckBox("Espresso");
         nameCheckBox[2] = new JCheckBox("Mocca");
         nameCheckBox[3] = new JCheckBox("Latte");
         nameCheckBox[4] = new JCheckBox("Americano");
         nameCheckBox[5] = new JCheckBox("Coco");
-        nameCheckBox[5] = new JCheckBox("Milk Green Tea");
-        nameCheckBox[6] = new JCheckBox("Thai Tea");
-        nameCheckBox[7] = new JCheckBox("Milk");
-        nameCheckBox[8] = new JCheckBox("Cake");
-        nameCheckBox[9] = new JCheckBox("Cookie");
-        nameCheckBox[10] = new JCheckBox("Pancake");
-        nameCheckBox[11] = new JCheckBox("Muffin");
+        nameCheckBox[6] = new JCheckBox("Milk Green Tea");
+        nameCheckBox[7] = new JCheckBox("Thai Tea");
 
-        quantity[0] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[1] = new JSpinner(new SpinnerNumberModel(0,0,3,1));
-        quantity[2] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[3] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[4] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[5] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[6] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[7] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[8] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[9] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[10] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-        quantity[11] = new JSpinner(new SpinnerNumberModel(0,0,5,1));
-
-
-        slipPanel = new JTextArea(20, 50);
-        slipPanel.setEditable(false);
-        slipPanel.append("Hello, world!\n");
-        slipPanel.append("This is a message in Terminal.\n");
-        slipPanel.append("Type your commands here.\n");
-
-
-        JPanel panel = new JPanel(new GridBagLayout());
-        panel.add(titleLabel);
-        for(int i = 0 ; i<12;i++){
-            panel.add(nameCheckBox[i]);  
-            panel.add(quantity[i]);  
-        }          
-
-        // gbc.gridy++;
-        // gbc.gridwidth = 100;
-        // panel.add(slipPanel, gbc);
-        add(panel);
+        JPanel coffeePanel = new JPanel(new GridLayout(4, 2, 10, 10));
+        coffeePanel.setBorder(BorderFactory.createTitledBorder("Coffee"));
+        
+        JPanel capuccinoPanel = new JPanel(new BorderLayout());
+        capuccinoPanel.add(nameCheckBox[0], BorderLayout.SOUTH);
+        coffeePanel.add(capuccinoPanel);
+        
+        JPanel espressoPanel = new JPanel(new BorderLayout());
+        espressoPanel.add(nameCheckBox[1], BorderLayout.SOUTH);
+        coffeePanel.add(espressoPanel);
+        
+        JPanel moccaPanel = new JPanel(new BorderLayout());
+        moccaPanel.add(nameCheckBox[2], BorderLayout.SOUTH);
+        coffeePanel.add(moccaPanel);
+        
+        JPanel lattePanel = new JPanel(new BorderLayout());
+        lattePanel.add(nameCheckBox[3], BorderLayout.SOUTH);
+        coffeePanel.add(lattePanel);
+        
+        JPanel americanoPanel = new JPanel(new BorderLayout());
+        americanoPanel.add(nameCheckBox[4], BorderLayout.SOUTH);
+        coffeePanel.add(americanoPanel);
+        
+        add(coffeePanel);
     }
 
-    
     public static void main(String[] args) throws Exception {
         new Dashboard().setVisible(true);
     }
