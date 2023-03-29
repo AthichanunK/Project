a694d2fd0ff55e38b5c3e0f8ce7274c7e80518c8
whysoakemiii <sujira.jamdee@gmail.com>
2023-03-29 19:42:55 +0700

    Update

--- CafeManagemnet/src/Dashboard.java
+++ CafeManagemnet/src/Dashboard.java
@@ -4,12 +4,12 @@ import java.awt.event.*;
 
 public class Dashboard extends JFrame{
     private JLabel titleLabel;
-    private JLabel name;
-    private JLabel price;
-    private JLabel quantity;
-    private JLabel purchase;
-
-    private JLabel slipPanel;
+    private JLabel[] nameLabels = new JLabel[10];
+    private JLabel[] priceLabel = new JLabel[10];
+    private JLabel[] quantityLabel = new JLabel[10];
+    private JLabel[] purchaseLabel = new JLabel[10];
+    private JCheckBox[] purchasCheckBox = new JCheckBox[10];
+    private JTextArea slipPanel;
     private JButton totalButton;
     private JButton receipButton;
     private JButton resetButton;
@@ -20,6 +20,18 @@ public class Dashboard extends JFrame{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setUndecorated(true);
+
+        titleLabel = new JLabel("Sunny Side Up! Cafe");
+        titleLabel.setFont(new Font("Bahnschrift", Font.BOLD, 30)); 
+
+        slipPanel = new JTextArea(20, 50);
+
+
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        // GridBagConstraints gbc = new GridBagConstraints();
+        panel.add(titleLabel);
+        add(panel);
     }
 
     
